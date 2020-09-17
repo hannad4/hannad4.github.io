e4ffc5fd33d092f766484b7759e8c8e5b830ba71
Danny Hanna <hannad4@tcnj.edu>
2020-09-17 19:43:52 -0400

    creating navigation framework

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(); 
+    this.state = {
+      title: "Danny's Website", 
+      headerLinks: [
+        {title: 'Home', path: '/'},
+        {title: 'Projects', path: '/projects'},
+        {title: 'Documents', path: '/documents'},
+        {title: 'Contact Me', path: '/contact'}
+      ], 
+      home: {
+        title: 'Hey there!', 
+        subtitle: 'Welcome to my personal website.',
+        text: 'Placeholder text for now',
+      }, 
+      projects: {
+        title: 'My Projects', 
+        subtitle: "From app development to tinkering with robots, I've tried it all", 
+      }, 
+      documents: {
+        title: 'So you wanna see my deets, huh?', 
+      }, 
+      contact: {
+        title: "Let's get in touch",
+      }
+
+    }
+  }
+
+  render() {
+    return (
+      <div>Hello testing</div>
+    );
+  }
+  
 }
 
 export default App;
