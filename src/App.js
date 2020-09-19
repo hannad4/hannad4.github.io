5ebd24794ce1444554b676f51ca3ce1a7c376134
Danny Hanna <hannad4@tcnj.edu>
2020-09-19 11:06:00 -0400

    created navbar & added dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'; 
 
 class App extends React.Component {
   constructor(props) {
@@ -34,7 +42,46 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>Hello testing</div>
+      <Router> 
+        <Container fluid={true} className='p-0'>  { /*Fluid false = Huge left margin. Change padding via "className='p-0'" */ }
+          {/* <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                  <Link to="/documents">Documents</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact Me</Link>
+                </li>
+              </ul>
+            </nav>
+          </div> */}
+
+          <Navbar expand="sm" bg="light" variant="light" className="border-bottom">
+            <Navbar.Brand>
+              Daniel Hanna
+            </Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/projects">Projects</Link>
+                <Link className="nav-link" to="/documents">Documents</Link>
+                <Link className="nav-link" to="/contact">Contact Me</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+        </Container>
+      </Router>
+      
     );
   }
   
