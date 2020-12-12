57d5e29a4f759a7ebb7452c347a5e300d27958de
Danny Hanna <hannad4@tcnj.edu>
2020-12-12 13:53:46 -0500

    fixing repo link

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -13,7 +13,7 @@ function ProjectsPage(props) {
                     <Col xs={8} className="p-0">
                         <br></br>
                         {props.title && <h1 className="display-4 font-weight-bold">{props.title}</h1>}
-                        <p className="display-6 font-weight-light">These are just some highlights. You can find some more good ones <b><a href="https://github.com/hannad4" target="_blank" rel="noopener noreferrer">here</a></b> 😉</p>
+                        <p className="display-6 font-weight-light">These are just some highlights. You can find some more good ones <b><a href="https://github.com/hannad4?tab=repositories" target="_blank" rel="noopener noreferrer">here</a></b> 😉</p>
                     </Col>
                     <ProjectsCarousel></ProjectsCarousel>
                     </center>
