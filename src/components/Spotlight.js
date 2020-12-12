b44dbe5f63da53da931fd394087730cab4ad798a
Danny Hanna <hannad4@tcnj.edu>
2020-12-12 12:35:24 -0500

    adding final touches

--- src/components/Spotlight.js
+++ src/components/Spotlight.js
@@ -39,15 +39,8 @@ function Spotlight(props) {
                 <footer id="footer">
                     <Container className="border-top" fluid={true}>
                         <div className="p-2" style={{ textAlign: 'center' }}>
-                            <small>This site was made with ❤ using ReactJS & Bootstrap. For the best experience, visit this site full-screened on desktop. Check out the source code: </small>
-                            <a href="https://github.com/hannad4/my-website" target="_blank" rel="noopener noreferrer">
-                                <img
-                                    alt="Link to website source code located on my GitHub page"
-                                    src="/githubLogo.png"
-                                    width="15px"
-                                    height="15px"
-                                    className="d-inline-block align-left" />{' '}
-                            </a>
+                            <small>This site was made with ❤ using ReactJS & Bootstrap. For the best experience, visit this site full-screened on desktop. Check out the source code <a href="https://github.com/hannad4/my-website" target="_blank" rel="noopener noreferrer">here</a>
+                            </small>
                         </div>
                     </Container>
                 </footer>
