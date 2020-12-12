9e2d12506384808600ffaa74b9070b1c16bf7584
Danny Hanna <hannad4@tcnj.edu>
2020-12-12 13:00:18 -0500

    ready for deployment

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,10 +7,10 @@ function AboutMe() {
             Welcome to my website! My name is Daniel Hanna. I am a Cum Laude graduate from The College of New Jersey with a Bachelors of Science in Computer Engineering. I have experience in both Computer and Software Engineering. I also find great interest in Digial Circuits, Mircoprocessors, and Operating Systems Concepts. 
             <br></br>
             <br></br>
-            I am currently a Network Operations Engineer in Long Distance Transport through Verizon's Leadership Development Program. I help plan and provision circuits on Verizon's network in order to satisfy customer requests and requirements. You can learn more through my Resume located on this site's <i>Documents page</i>.
+            I am currently a Network Operations Engineer in Long Distance Transport through Verizon's Leadership Development Program. I help plan and provision circuits on Verizon's network in order to satisfy customer requests and requirements.
             <br></br>
             <br></br>
-            I have worked on numerous projects that are available for viewing in the projects tab seen at the top of this page. If you are interested in learning more, I encourage you to check out the rest of this website. You can always shoot me a message via the <i>Contact Me</i> form located on this website as well!
+            I have worked on numerous projects that are available for viewing in the projects tab seen at the top of this page. If you are interested in learning more about me, I encourage you to check out the rest of this website. You can find a link to email me via the <i>Contact Me</i> page on this website as well!
         </p>
     ); 
 }
